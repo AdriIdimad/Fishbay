048c06e2446bbc30d5758d8b81f9a3498da15836
Miguel Alonso <miguelponfe98@gmail.com>
2017-07-20 12:17:48 +0200

    +  FIREBASE + CFG +  Solo iniciado

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAZrGptx0aL4AmILAg6U9WcodJ23D8bLkU",
+    authDomain: "fishbay-912f5.firebaseapp.com",
+    databaseURL: "https://fishbay-912f5.firebaseio.com",
+    projectId: "fishbay-912f5",
+    storageBucket: "fishbay-912f5.appspot.com",
+    messagingSenderId: "944185704251"
+  };
