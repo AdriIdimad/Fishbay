b4507087b30b4b46911a8938dfb3f2f507f0996b
Adri <avelazquezdiego@gmail.com>
2017-07-25 09:41:41 +0200

    espacio

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -29,7 +29,6 @@ export class LoginPage {
         const fc=firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken)
         firebase.auth().signInWithCredential(fc).then(fs=>{
           alert("firebase sec")
-          
       }).catch(err=>{
         alert("firebase erro")
       })
@@ -62,5 +61,6 @@ export class LoginPage {
     lostpass(){
     this.navCtrl.push('LostpassPage')
   }
+
 
 }
