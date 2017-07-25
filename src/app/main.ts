978ab02430af33c5f1a0c2672d6f038ee45ab9b1
Miguel Alonso <miguel@zatmar.me>
2017-07-25 11:52:20 +0000

    arregllo conflicto

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,17 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 import { enableProdMode } from "@angular/core";
+import 'rxjs/add/operator/take';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 enableProdMode();
-=======
-import 'rxjs/add/operator/take';
-
->>>>>>> 951a060a0ef6b31987ad19497e444bade4eb8fe1
-=======
-import 'rxjs/add/operator/take';
-
->>>>>>> 951a060a0ef6b31987ad19497e444bade4eb8fe1
 platformBrowserDynamic().bootstrapModule(AppModule);
 
